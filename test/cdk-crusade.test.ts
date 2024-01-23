03433be62706bf380bd12ed122d27e939e07ad41
Jeff Horton <jeff@goblinoid.co.uk>
2024-01-23 00:45:21 +0000

    Initial commit

---
+++ test/cdk-crusade.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCrusade from '../lib/cdk-crusade-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-crusade-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCrusade.CdkCrusadeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
